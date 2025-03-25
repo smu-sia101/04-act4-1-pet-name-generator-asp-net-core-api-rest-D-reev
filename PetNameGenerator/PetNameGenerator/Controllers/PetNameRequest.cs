59f86b0281371e8df2214cf19cd4fca38b7a07ab
AndreevAbellera <AndreevAbellera@SMU-0043>
2025-03-25 18:25:39 +0800

    Updated PetNameController #2

---
+++ PetNameGenerator/PetNameGenerator/Controllers/PetNameRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PetNameGenerator.Controllers
+{
+    public class PetNameRequest
+    {
+        public string AnimalType { get; set; }
+        public bool? TwoPart { get; set; }
+    }
+}
